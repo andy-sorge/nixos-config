e1564e8505a3c4ba9049359b207987ccfd6f6d7a
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-03-05 15:24:43 -0800

    remove dev tools that should be in shells instead of global

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -1,23 +1,14 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
   home.packages = with pkgs; [
     # utils
     gh
-    
+
     # editors
     zed-editor
-    
-    # c/c++
-    gcc
-    cmake
-    gdb
-    
+
     # nix
     nil
     nixd
-    
-    # rust
-    cargo
-    rustc
   ];
 }
