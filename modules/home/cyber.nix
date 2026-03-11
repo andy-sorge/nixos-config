5fef3a2046e40cd7a8c59dcead25ff739b95b995
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-03-10 17:02:00 -0700

    cybersecurity utils

--- modules/home/cyber.nix
+++ modules/home/cyber.nix
@@ -1,13 +1,69 @@
-{pkgs, ...}:
-{
+{ pkgs, ... }:
+{  
   home.packages = with pkgs; [
-    # cli
-    binutils
-    exiftool
+    # e&e
+    metasploit
+    armitage
+    dig
+    sslscan
+    evil-winrm
+    evil-winrm-py
+    sqlmap
+    yara
+    
+    # reverse
+    ghidra
+    radare2
+    
+    # web
+    burpsuite
+    dirbuster
+    dirstalk
+    wpscan
+    subfinder
+    cadaver
+    dirb
+    ffuf
+    
+    # network
+    nmap
+    aircrack-ng
+    traceroute
+    netcat
+    tcpdump
+    wireshark
+    macchanger
+    airgeddon
+    arping
+    arp-scan
+    netdiscover
+    
+    # password
+    hashcat
+    john
+    hydra
+    crunch
+    cewl
+    
+    # forensic
     binwalk
     foremost
+    binutils
+    steghide
+    testdisk
+    imhex
     
-    #gui
-    wireshark
+    # osint
+    sherlock
+    exiftool
+    dnsrecon
+    
+    # not working yet
+    # 
+    # mitmproxy
+    # 
+    # ophcrack
+    expat # required for ophcrack
+    
   ];
 }
