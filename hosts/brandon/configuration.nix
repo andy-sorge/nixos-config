cd15be7fc4a3913b93dc005e679118f3e048e339
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-03-14 22:46:59 -0700

    move to common

--- hosts/brandon/configuration.nix
+++ hosts/brandon/configuration.nix
@@ -3,12 +3,9 @@
   imports = [
     ./hardware-configuration.nix
   ];
-  
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  
+
   networking.hostName = "brandon";
-  
+
   # dont touch this idiot
   system.stateVersion = "25.05";
 }
