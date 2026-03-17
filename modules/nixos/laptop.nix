63a636bf016b0afa62a12a9494e1f07a4c0fc164
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-03-17 00:52:10 -0700

    disable tlp for plasma, add fingerprint reader

--- modules/nixos/laptop.nix
+++ modules/nixos/laptop.nix
@@ -4,14 +4,17 @@
   powerManagement.enable = true;
   
   # configurable power manager service
-  services.tlp = {
-    enable = true;
-  };
+  # services.tlp = {
+  #   enable = true;
+  # };
   
   services = {
     gvfs.enable = true;
     upower.enable = true;
     thermald.enable = true;
+    
+    fprintd.enable = true;
+    fprintd.tod.driver = pkgs.libfprint-2-tod1-goodix;
   };
   
   hardware.bluetooth = {
