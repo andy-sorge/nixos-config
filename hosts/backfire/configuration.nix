3473522f969021adc500433acfac8b11bec22456
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-07-21 12:36:00 -0700

    backfire: nvidia config

--- hosts/backfire/configuration.nix
+++ hosts/backfire/configuration.nix
@@ -6,6 +6,18 @@
 
   networking.hostName = "backfire";
 
+  hardware.graphics.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    modesetting.enable = true;
+
+    powerManagement.enable = false;
+
+    open = true;
+
+    nvidiaSettings = true;
+  };
+
   # dont touch this idiot
   system.stateVersion = "25.05";
 }
