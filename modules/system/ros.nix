9d6b770ddee9c092180ef50a3cae0e0d6a2cf8aa
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-07-20 23:55:32 -0700

    comment

--- modules/system/ros.nix
+++ modules/system/ros.nix
@@ -2,6 +2,8 @@
 {
   networking.firewall = {
     enable = true;
+    
+    # allow necessary ports for ros2 networking
     allowedUDPPorts = [ 7400 7401 ];
     allowedUDPPortRanges = [
       { from = 7410; to = 7500; }
