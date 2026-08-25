1ba3754d9d0108cf74335bc5ef3ad8c61bdd282b
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-08-25 13:41:33 -0700

    Add git comfig to hm

--- modules/home/development.nix
+++ modules/home/development.nix
@@ -13,4 +13,39 @@
     nixd
     nixfmt
   ];
+
+  programs.git = {
+    enable = true;
+  
+    signing = {
+      key = "/home/andy/.ssh/id_ed25519_sk_rk.pub";
+      signByDefault = true;
+    };
+  
+    settings = {
+      user = {
+        name = "Andy Sorge";
+        email = "124544413+andy-sorge@users.noreply.github.com";
+      };
+      
+      gpg.format = "ssh";
+      init.defaultBranch = "main";
+  
+      filter.lfs = {
+        clean = "git-lfs clean -- %f";
+        smudge = "git-lfs smudge -- %f";
+        process = "git-lfs filter-process";
+        required = true;
+      };
+  
+      credential."https://github.com".helper = [
+        ""
+        "!/etc/profiles/per-user/andy/bin/gh auth git-credential"
+      ];
+      credential."https://gist.github.com".helper = [
+        ""
+        "!/etc/profiles/per-user/andy/bin/gh auth git-credential"
+      ];
+    };
+  };
 }
