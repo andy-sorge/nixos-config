e4be22ab8d0b5d126996340e28d560f8472041ec
Andy Sorge <124544413+andy-sorge@users.noreply.github.com>
2026-09-11 16:18:27 -0700

    correct fish module path

--- hosts/fulcrum/home.nix
+++ hosts/fulcrum/home.nix
@@ -10,7 +10,7 @@
     ../../modules/home/nixvim.nix
     ../../modules/home/vscode.nix
     ../../modules/home/ssh.nix
-    ../../modules/home/fish.nix
+    ../../modules/home/fish
     
     ../../modules/home/cyber.nix
     ../../modules/home/development.nix
